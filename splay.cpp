b9899b46fff80ca4a9d21f6b561f2543614386af
Noam527 <86576579+Noam527@users.noreply.github.com>
2021-06-30 23:06:51 +0300

    Update splay.cpp

--- splay.cpp
+++ splay.cpp
@@ -1,401 +1,183 @@
-// edit this with lazy values.
-template<bool lazy, typename T = int>
-struct node {
-	node<lazy, T> *l, *r, *p;
-	T value;
-	// normal values. keep size!
-	int size;
-	// lazy values, for instance:
-	T add;
-	node() {}
-	node(T val) {
-		l = r = p = nullptr;
-		value = val;
-		size = 1;
-		add = 0;
-	}
-	void push() {
-		if (!lazy) return;
-		if (add) {
-			if (l) l->add += add;
-			if (r) r->add += add;
-			value += add;
-			add = 0;
-		}
-	}
-	// a call on this assumes the current node has been pushed.
-	void fix() {
-		if (lazy) {
-			if (l) l->push();
-			if (r) r->push();
-		}
-		size = 1;
-		if (l) size += l->size;
-		if (r) size += r->size;
-	}
-	~node<lazy, T>() {
-		if (l) delete l;
-		if (r) delete r;
-	}
-};
-
-template<bool lazy, typename T = int>
 struct splaytree {
-	typedef node<lazy, T> node;
-	node *root;
-	splaytree() {
-		root = nullptr;
+	// to be nonlazy, if efficiency is necessary, remove the definition of push() and uncomment the next line:
+	//#define push(X) ;
+	struct node {
+		int ch[2] = {}, p;
+		ll val, mn; // hold some values
+		int size;
+		ll add; // hold some lazy tags
+		node(ll value = 0) {
+			ch[0] = ch[1] = p = add = 0;
+			size = 1;
+			val = mn = value;
+		}
+		int& operator [](int index) {
+			return ch[index];
+		}
+	};
+	int root = 0; // will hold some root.
+	vector<node> T;
+	vector<int> free_nodes;
+	splaytree(int expected_max) {
+		T.reserve(expected_max + 1);
+		// initialize T[0] with null values:
+		T.push_back(node());
+		T[0].mn = inf;
+		T[0].size = 0;
 	}
-	splaytree(node *r) {
-		root = r;
+	int new_node(ll value) {
+		if (free_nodes.size()) {
+			int res = free_nodes.back();
+			free_nodes.pop_back();
+			T[res] = node(value);
+			return res;
+		}
+		T.push_back(node(value));
+		return (int)T.size() - 1;
 	}
-	// assumes y and its relevant child pushed.
-	void rright(node *y) {
-		node *x = y->l, *b = x->r, *d = y->p;
-		if (lazy) {
-			if (d) {
-				d->push();
-				y->push();
-				x->push();
-			}
+	// update push and fix accordingly
+	void push(int x) {
+		if (!x) return;
+		int l = T[x][0], r = T[x][1];
+		if (T[x].add) {
+			T[x].val += T[x].add;
+			T[x].mn += T[x].add;
+			T[l].add += T[x].add, T[r].add += T[x].add;
+			T[x].add = 0;
 		}
-		if (d) {
-			if (d->l == y) d->l = x;
-			else d->r = x;
-		}
-		y->p = x;
-		x->p = d;
-		x->r = y;
-		y->l = b;
-		if (b) b->p = y;
-		y->fix();
-		x->fix();
-		if (d) d->fix();
 	}
-	// assumes y and its relevant child pushed.
-	void rleft(node *y) {
-		node *x = y->r, *b = x->l, *d = y->p;
-		if (lazy) {
-			if (d) {
-				d->push();
-				y->push();
-				x->push();
-			}
-		}
-		if (d) {
-			if (d->l == y) d->l = x;
-			else d->r = x;
-		}
-		y->p = x;
-		x->p = d;
-		x->l = y;
-		y->r = b;
-		if (b) b->p = y;
-		y->fix();
-		x->fix();
-		if (d) d->fix();
+	void fix(int x) {
+		if (!x) return;
+		int l = T[x][0], r = T[x][1];
+		push(l); push(r);
+		T[x].size = 1 + T[l].size + T[r].size;
+		T[x].mn = min(min(T[x].val, T[l].mn), T[r].mn); // works fine for 0 because T[0].mn = inf.
 	}
-	// guarantees that the root pushed.
-	node* splay(node *x) {
-		if (x == nullptr) return nullptr;
-		node *p, *pp;
-		while ((p = x->p) != nullptr) {
-			pp = p->p;
-			if (lazy) {
-				if (pp) pp->push();
-				p->push();
-				x->push();
-			}
-			if (!pp) {
-				if (p->l == x) rright(p);
-				else rleft(p);
-				break;
-			}
-			if (pp->l == p) {
-				if (p->l == x)
-					rright(pp), rright(p);
-				else
-					rleft(p), rright(pp);
-			}
-			else {
-				if (p->l == x)
-					rright(p), rleft(pp);
-				else
-					rleft(pp), rleft(p);
-			}
+	// assumes p is pushed.
+	void setchild(int p, int d, int x) {
+		T[p][d] = x, T[x].p = p;
+		fix(p);
+	}
+	int dir(int x) {
+		int p = T[x].p;
+		if (!p) return -1;
+		return T[T[x].p][1] == x ? 1 : 0;
+	}
+	// assumes the chain (x, x.p, x.p.p) is all pushed.
+	void rotate(int x) {
+		int y = T[x].p, z = T[y].p, dx = dir(x), dy = dir(y);
+		setchild(y, dx, T[x][!dx]);
+		setchild(x, !dx, y);
+		if (dy >= 0) setchild(z, dy, x);
+		else T[x].p = z; // needs to happen regardless.
+	}
+	// guarantees the new root is pushed.
+	int splay(int x) {
+		for (push(x); T[x].p;) {
+			int y = T[x].p, z = T[y].p;
+			push(z); push(y); push(x);
+			int dx = dir(x), dy = dir(y);
+			if (dy >= 0) rotate(dx == dy ? y : x);
+			rotate(x);
 		}
-		x->push();
-		x->fix();
 		return root = x;
 	}
-
-	// basic operations: some re-implement ideas for better constant factors.
-	int size() {
-		return root ? root->size : 0;
-	}
-	// get node by index: from 0 to size() - 1. guarantees that the root pushed.
-	node* index(int k) {
-		if (k < 0 || size() <= k) {
-			cerr << "splaytree index: tried to access " << k << " while the size was " << size() << '\n';
-			return nullptr;
-		}
-		node *x = root;
-		while (true) {
-			x->push();
-			int lsz = x->l ? x->l->size : 0;
-			if (k == lsz)
-				return splay(x);
-			if (k < lsz)
-				x = x->l;
+	// have at it
+	// returns the new node at root
+	int lowerbound(ll val, int start) {
+		int v = 0, lst = 0;
+		for (int cur = (start ? start : root); cur;) {
+			push(cur);
+			lst = cur;
+			if (T[cur].val < val)
+				cur = T[cur][1];
 			else
-				x = x->r, k -= (lsz + 1);
+				v = cur, cur = T[cur][0];
 		}
-		return nullptr; // shouldn't reach here
+		if (!v) return splay(lst);
+		return splay(v);
 	}
-	// split by value (<= x, > x)
-	pair<splaytree, splaytree> split(T x) {
-		if (!root) {
-			return{ splaytree(), splaytree()};
-		}
-		node *z = root;
-		while (true) {
-			z->push();
-			if (z->value <= x) {
-				if (z->r) z = z->r;
-				else break;
-			}
-			else {
-				if (z->l) z = z->l;
-				else break;
-			}
-		}
-		splay(z);
-		splaytree B, C;
-		if (z->value <= x) {
-			B = splaytree(z), C = splaytree(z->r);
-			if (z->r) {
-				z->r->p = nullptr;
-				z->r = nullptr;
-			}
-		}
-		else {
-			B = splaytree(z->l), C = splaytree(z);
-			if (z->l) {
-				z->l->p = nullptr;
-				z->l = nullptr;
-			}
-		}
-		z->fix();
-		return{ B, C };
+	// the actual value
+	ll lowerbound(ll val) {
+		int v = lowerbound(val, root);
+		if (T[v].val >= val) return T[v].val;
+		return inf;
 	}
-	// split by size (size k, rest of them).
-	pair<splaytree, splaytree> sizesplit(int k) {
-		if (k == 0) return{ splaytree(), splaytree(root) };
-		if (k == size()) return{ splaytree(root), splaytree() };
-		node *x = index(k);
-		splaytree B(x->l), C(x);
-		if (x->l) {
-			x->l->p = nullptr;
-			x->l = nullptr;
-		}
-		x->fix();
-		return{ B, C };
-	}
-	// merges the current tree with B from the right.
-	node* merge(splaytree B) {
-		if (!B.root) return root;
-		if (!root) return root = B.root;
-		node *x = root;
-		if (lazy) x->push();
-		while (x->r) {
-			x = x->r;
-			if (lazy) x->push();
-		}
-		splay(x);
-		root->r = B.root;
-		if (B.root) B.root->p = root;
-		root->fix();
-		return root;
-	}
-	// roots the tree at the first node with value >= x (and returns it).
-	// if none exists, returns null (the root changes though).
-	node* lowerbound(T x) {
-		if (!root) return nullptr;
-		node *z = root, *lst = nullptr;
-		while (true) {
-			if (lazy) z->push();
-			if (z->value < x) {
-				if (z->r) z = z->r;
-				else break;
+	// returns the new node at root
+	int index(int k, int start) {
+		while (1) {
+			push(start);
+			int l = T[start][0];
+			if (T[l].size > k) start = l;
+			else if (T[l].size < k) {
+				start = T[start][1];
+				k -= (1 + T[l].size);
 			}
-			else {
-				lst = z;
-				if (z->l) z = z->l;
-				else break;
-			}
+			else return splay(start);
 		}
-		if (lst == nullptr) {
-			splay(z);
-			return nullptr;
-		}
-		return splay(lst);
 	}
-	// roots the tree at the first node with value > x (and returns it).
-	// if none exists, returns null.
-	node* upperbound(T x) {
-		if (!root) return nullptr;
-		node *z = root, *lst = nullptr;
-		while (true) {
-			if (lazy) z->push();
-			if (z->value <= x) {
-				if (z->r) z = z->r;
-				else break;
-			}
-			else {
-				lst = z;
-				if (z->l) z = z->l;
-				else break;
-			}
-		}
-		if (lst == nullptr) {
-			splay(z);
-			return nullptr;
-		}
-		return splay(lst);
+	// the actual value
+	ll index(int k) {
+		return T[index(k, root)].val;
 	}
-	// number of elements strictly less than / at most some x.
-	int lessthan(T x) {
-		if (!root) return 0;
-		node *z = lowerbound(x);
-		if (z == nullptr) return size();
-		return z->l ? z->l->size : 0;
+	// splits the splay containing v into two splays with roots l, r:
+	// l has all values < val, r has all values >= val.
+	void split_value(int v, int &l, int &r, ll val) {
+		splay(v);
+		int u = lowerbound(val, v);
+		if (T[u].val < val) {
+			l = v, r = 0;
+			return;
+		}
+		r = u, l = T[u][0];
+		setchild(u, 0, 0);
+		if (l) T[l].p = 0;
 	}
-	int atmost(T x) {
-		if (!root) return 0;
-		node *z = upperbound(x);
-		if (z == nullptr) return size();
-		return z->l ? z->l->size : 0;
+	void split_size(int v, int &l, int &r, int lsz) {
+		splay(v);
+		if (T[v].size == lsz) {
+			l = v, r = 0;
+			return;
+		}
+		int u = index(lsz, v);
+		r = u, l = T[u][0];
+		setchild(u, 0, 0);
+		if (l) T[l].p = 0;
+	}
+	// merge the trees rooted at l, r, return the new root.
+	int merge(int l, int r) {
+		if (!l || !r) return l | r;
+		for (push(l); T[l][1]; push(l))
+			l = T[l][1];
+		splay(l);
+		setchild(l, 1, r);
+		return l;
 	}
 
-	// build upon the basic operations:
-	node* insert(T value) {
-		if (!root) return root = new node(value);
-		pair<splaytree, splaytree> X = split(value);
-		node *n = new node(value);
-		n->l = X.first.root, n->r = X.second.root;
-		if (X.first.root) X.first.root->p = n;
-		if (X.second.root) X.second.root->p = n;
-		n->fix();
+	int insert(ll val) {
+		int l, r, n = new_node(val);
+		split_value(root, l, r, val);
+		setchild(n, 0, l), setchild(n, 1, r);
 		return root = n;
 	}
-	// after the insertion, the value will be at index k.
-	node* insert_at(T value, int k) {
-		if (!root) return root = new node(value);
-		pair<splaytree, splaytree> X = sizesplit(k);
-		node *n = new node(value);
-		n->l = X.first.root, n->r = X.second.root;
-		if (X.first.root) X.first.root->p = n;
-		if (X.second.root) X.second.root->p = n;
-		n->fix();
+	int insert_at(ll val, int k) {
+		int l, r, n = new_node(val);
+		split_size(root, l, r, k);
+		setchild(n, 0, l), setchild(n, 1, r);
 		return root = n;
 	}
-	void erase(T value) {
-		node *x = lowerbound(value);
-		splaytree A(x->l), B(x->r);
-		if (x->l) x->l->p = nullptr;
-		if (x->r) x->r->p = nullptr;
-		x->l = x->r = nullptr;
-		delete x;
-		root = A.merge(B);
+	void erase(ll val) {
+		int v = splay(lowerbound(val, root));
+		free_nodes.push_back(v);
+		int l = T[v][0], r = T[v][1];
+		T[l].p = T[r].p = 0;
+		root = merge(l, r);
 	}
 	void erase_at(int k) {
-		node *x = index(k);
-		splaytree A(x->l), B(x->r);
-		if (x->l) x->l->p = nullptr;
-		if (x->r) x->r->p = nullptr;
-		x->l = x->r = nullptr;
-		delete x;
-		root = A.merge(B);
-	}
-	// add your own.
-
-
-	void gettree(node *x, vector<T> &result) {
-		if (!x) return;
-		x->push();
-		gettree(x->l, result);
-		result.push_back(x->value);
-		gettree(x->r, result);
-	}
-	vector<T> gettree() {
-		vector<T> result;
-		gettree(root, result);
-		return result;
+		int v = splay(index(k, root));
+		free_nodes.push_back(v);
+		int l = T[v][0], r = T[v][1];
+		T[l].p = T[r].p = 0;
+		root = merge(l, r);
 	}
 };
-
-void solve() {
-	splaytree<0> T;
-	while (1) {
-		string op;
-		cin >> op;
-		if (op == "rst") {
-			T = splaytree<0>();
-		}
-		else if (op == "insert") {
-			int x;
-			cin >> x;
-			T.insert(x);
-		}
-		else if (op == "insertat") {
-			int k, x;
-			cin >> k >> x;
-			T.insert_at(x, k);
-		}
-		else if (op == "erase") {
-			int x;
-			cin >> x;
-			T.erase(x);
-		}
-		else if (op == "eraseat") {
-			int k;
-			cin >> k;
-			T.erase_at(k);
-		}
-		else if (op == "lowerbound") {
-			int x;
-			cin >> x;
-			node<0> *z = T.lowerbound(x);
-			if (!z) cout << "null\n";
-			else cout << z->value << '\n';
-		}
-		else if (op == "upperbound") {
-			int x;
-			cin >> x;
-			node<0> *z = T.upperbound(x);
-			if (!z) cout << "null\n";
-			else cout << z->value << '\n';
-		}
-		else if (op == "lessthan") {
-			int x;
-			cin >> x;
-			cout << T.lessthan(x) << '\n';
-		}
-		else if (op == "index") {
-			int k;
-			cin >> k;
-			node<0> *z = T.index(k);
-			if (!z) cout << "null\n";
-			else cout << z->value << '\n';
-		}
-		else if (op == "size") {
-			cout << T.size() << '\n';
-		}
-
-
-		cout << "fixed: " << T.isfixed() << '\n';
-		vector<int> t = T.gettree();
-		for (const auto &i : t) cout << i << " "; cout << '\n';
-	}
-
-}
