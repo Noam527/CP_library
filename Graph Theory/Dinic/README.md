0abb542623984b62f72e35413a482b3458dd92cb
Noam Gutterman <noam.g527@gmail.com>
2024-09-07 15:36:13 +0300

    Dinitz implementation + README

---
+++ Graph Theory/Dinic/README.md
@@ -0,0 +1,18 @@
+### Problem Statement
+Find maximum flow in a graph using dinitz.
+
+### Complexity
+- **Time**: $O(VE \log U)$ in general graphs, $O(E \sqrt E)$ in unit capacity graphs, $O(E \sqrt V)$ in maximum matching graphs.
+- **Memory**: Linear.
+
+### Core Ideas
+Uses dinitz with capacity scaling - first does scaling, then performs the entire dinitz. Thus at every iteration of scaling, the maximum flow is bounded by $E$, and so dinitz runs in $O(V\cdot f) = O(VE)$, for a total of $O(VE \log U)$.<br>
+In unit capacity graphs, $O(E \sqrt E)$ since there are $O(\sqrt E)$ phases of blocking flow, and each blocking flow takes $O(E)$.<br>
+In graphs where each vertex allows at most one path, $O(E \sqrt V)$, since there are $O(\sqrt V)$ phases of blocking flow.
+
+### Tests and Benchmarks
+- Maximum flow: https://www.spoj.com/status/FASTFLOW,noam527/
+- Maximum matching: https://www.spoj.com/status/MATCHING,noam527/
+
+### Source / Credit
+Inspired by https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/Dinic.h, but rewritten to my style.
--- Graph Theory/Dinic/dinic.cpp
+++ Graph Theory/Dinic/dinic.cpp
@@ -1,43 +1,80 @@
-// https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/Dinic.h
-struct dinic {
+/*
+* Dinitz's algorithm to compute maximum flow, with capacity scaling.
+* Template variable:
+*   - flow_t: Integer type big enough to store the flow on every edge and the maximum flow.
+*/
+template<typename flow_t = int>
+struct MaxFlow {
 	struct edge {
-		int to, rev;
-		ll c, oc;
-		ll flow() { return max(oc - c, 0LL); } // if you need flows
+		int to;
+		flow_t c;
+		edge(int to = -1, flow_t c = 0) : to(to), c(c) {}
 	};
-	vector<int> lvl, ptr, q;
-	vector<vector<edge>> adj;
-	dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
-	void addedge(int a, int b, ll c, ll rcap = 0) {
-		adj[a].push_back({ b, (int)adj[b].size(), c, c });
-		adj[b].push_back({ a, (int)adj[a].size() - 1, rcap, rcap });
+	int n;
+	vector<edge> edges;
+	vector<vector<int>> g; // indicies in edges list
+	MaxFlow(int n) : n(n) {
+		g.resize(n);
 	}
-	ll dfs(int v, int t, ll f) {
-		if (v == t || !f) return f;
-		for (int& i = ptr[v]; i < adj[v].size(); i++) {
-			edge& e = adj[v][i];
-			if (lvl[e.to] == lvl[v] + 1)
-				if (ll p = dfs(e.to, t, min(f, e.c))) {
-					e.c -= p, adj[e.to][e.rev].c += p;
-					return p;
+	void add_edge(int from, int to, flow_t cap, flow_t rev_cap = 0) {
+		g[from].push_back(edges.size());
+		edges.push_back(edge(to, cap));
+		g[to].push_back(edges.size());
+		edges.push_back(edge(from, rev_cap));
+	}
+	// dinic blocking flow (with required f)
+	bool bflow(vector<int>& curedge, const vector<int>& level, int v, int t, flow_t f) {
+		if (v == t) return true;
+		for (int& i = curedge[v]; i < g[v].size(); i++) {
+			int eind = g[v][i];
+			edge& e = edges[eind];
+			if (e.c >= f && level[e.to] == level[v] + 1) {
+				if (bflow(curedge, level, e.to, t, f)) {
+					e.c -= f;
+					edges[eind ^ 1].c += f;
+					return true;
 				}
-		}
-		return 0;
-	}
-	ll calc(int s, int t) {
-		ll flow = 0; q[0] = s;
-		for (int L = 0; L < 31; L++) do { // 'int L=30' maybe faster for random data
-			lvl = ptr = vector<int>(q.size());
-			int qi = 0, qe = lvl[s] = 1;
-			while (qi < qe && !lvl[t]) {
-				int v = q[qi++];
-				for (edge e : adj[v])
-					if (!lvl[e.to] && e.c >> (30 - L))
-						q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
 			}
-			while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
-		} while (lvl[t]);
-		return flow;
+		}
+		return false;
 	}
-	bool leftOfMinCut(int a) { return lvl[a] != 0; }
+	flow_t calc(int s, int t) {
+		flow_t result = 0;
+		flow_t D = numeric_limits<flow_t>::max();
+		while (D != (D & -D)) D ^= D & -D;
+
+		vector<int> level(n), curedge(n);
+		while (D > 0) {
+			do {
+				level.assign(n, -1);
+				curedge.assign(n, 0);
+				queue<int> q;
+				q.push(s);
+				level[s] = 0;
+				while (q.size()) {
+					int x = q.front();
+					q.pop();
+					for (const auto& i : g[x]) {
+						edge& e = edges[i];
+						if (e.c >= D && level[e.to] == -1) {
+							level[e.to] = level[x] + 1;
+							q.push(e.to);
+						}
+					}
+				}
+				while (bflow(curedge, level, s, t, D))
+					result += D;
+			} while (level[t] != -1);
+			D /= 2;
+		}
+		return result;
+	}
+	// example feature: get all edges from v s.t their final flow is 0.
+	vector<int> get_saturated_edges(int from) {
+		vector<int> result;
+		for (const auto& i : g[from])
+			if (edges[i].c == 0)
+				result.push_back(edges[i].to);
+		return result;
+	}
 };
