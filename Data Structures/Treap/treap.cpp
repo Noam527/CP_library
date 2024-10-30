81ee9b4509d8e03c0a7f80cc46f79aa39f0b287b
Noam527 <noam.g527@gmail.com>
2024-10-31 00:11:37 +0200

    Fixed treap (no push and fix on dummy node)

--- Data Structures/Treap/treap.cpp
+++ Data Structures/Treap/treap.cpp
@@ -1,66 +1,57 @@
-struct Treap {
-	static int trand() {
+struct treap {
+	static int trand() { // 30-bit random
 		return (rand() % (1 << 15)) | ((rand() % (1 << 15)) << 15);
 	}
 	struct node {
 		int l, r;
-		int size, pr;
-		ll val, mn; // IMP: values
+		int size, pr; // subtree size and random priority
+		ll val, mn; // IMP: node value and propagation values
 		ll add; // IMP: lazy tags
-		// IMP: default value for val (according to operation)
-		node(ll val = inf, int l = 0, int r = 0, int size = 1) : val(val), l(l), r(r), size(size) {
-			mn = val;
-			add = 0;
+		// IMP: default value for `val`L 0 for sum, inf for min
+		node(ll val = inf) : val(val), l(0), r(0), size(1) {
 			pr = trand();
+			mn = val, add = 0; // IMP: depends on your usage
 		}
-	};
+	}; // NOTICE: end of node
 	vector<node> t;
 	int root;
-	Treap() {
+	treap() {
 		srand(123123123); // IMP: choose seed
 		t.push_back(node()); // dummy
-		root = 0;
-		t[root].size = 0;
+		t[root = 0].size = 0;
 	}
 	int add_node(ll val) {
 		t.push_back(node(val));
 		return (int)t.size() - 1;
 	}
-	node& left(int n) {
-		return t[t[n].l];
-	}
-	node& right(int n) {
-		return t[t[n].r];
-	}
+	node& left(int n) { return t[t[n].l]; }
+	node& right(int n) { return t[t[n].r]; }
 	void push(int n) {
-		// IMP: lazy push. leave empty if treap is not lazy
+		if (!n) return; // dummy no-op
+		// IMP: apply lazy tag. leave empty if treap is not lazy
 		t[n].val += t[n].add, t[n].mn += t[n].add;
 		left(n).add += t[n].add;
 		right(n).add += t[n].add;
 		t[n].add = 0;
 	}
 	void fix(int n) {
+		if (!n) return; // dummy no-op
 		push(t[n].l), push(t[n].r);
 		t[n].size = 1 + left(n).size + right(n).size;
 		// IMP: fix values from children
 		t[n].mn = min(min(t[n].val, left(n).mn), right(n).mn);
 	}
 	int merge(int l, int r) {
-		if (!l || !r)
-			return l ^ r;
+		if (!l || !r) return l ^ r;
 		if (t[l].pr < t[r].pr) {
-			push(l);
-			t[l].r = merge(t[l].r, r);
-			fix(l);
+			push(l), t[l].r = merge(t[l].r, r), fix(l);
 			return l;
 		}
-		push(r);
-		t[r].l = merge(l, t[r].l);
-		fix(r);
+		push(r), t[r].l = merge(l, t[r].l), fix(r);
 		return r;
 	}
-	// IMP: if needed, operations by indicies
-	// split to l, r so that left size is `size`.
+	// OPTIONAL: 2 functions - operations by *indicies*
+	// split to l, r so that left size is `size`
 	void split_size(int n, int& l, int& r, int size) {
 		if (!n || !size) {
 			l = 0, r = n;
@@ -82,7 +73,7 @@ struct Treap {
 		split_size(root, l, r, index);
 		root = merge(merge(l, n), r);
 	}
-	// IMP: if needed, operations by values
+	// OPTIONAL: 2 functions - operations by *values*
 	// split to l, r so that everything in left is < val.
 	void split(int n, int& l, int& r, ll val) {
 		if (!n) {
